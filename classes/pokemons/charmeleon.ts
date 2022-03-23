52a43266e525e647b020225dd358cdbdd639af61
Stijndus <stijn@ardusit.nl>
2022-03-23 15:51:52 +0100

    Added 2 pokemons

---
+++ classes/pokemons/charmeleon.ts
@@ -0,0 +1,7 @@
+import { Pokemon } from "../pokemon";
+
+export class Charmeleon extends Pokemon{
+    constructor(name, health, type, attacks, resistance, weakness){
+        super(name, health, type, attacks, resistance, weakness);
+    }
+}
--- classes/pokemons/pikachu.ts
+++ classes/pokemons/pikachu.ts
@@ -1,7 +1,11 @@
+
 import { Pokemon } from "../pokemon";
+import { Resistances } from "../type/resistances";
+import { EnergyTypes } from "../type/types";
+import { Weaknesses } from "../type/weakness";
 
 export class Pikachu extends Pokemon{
-    constructor(name, health, type, attacks, resistance, weakness){
-        super(name, health, type, attacks, resistance, weakness);
+    constructor(name, health, attacks){
+        super(name, health, EnergyTypes.Electric, attacks, Resistances.Fighting, Weaknesses.Fire);
     }
 }
