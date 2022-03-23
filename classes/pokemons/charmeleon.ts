0b59edf74a867e11b67fda5511ffce874371ea9b
Stijndus <stijn@ardusit.nl>
2022-03-23 15:52:40 +0100

    Expanded Charmeleon class

--- classes/pokemons/charmeleon.ts
+++ classes/pokemons/charmeleon.ts
@@ -1,7 +1,11 @@
+import { Attack } from "../attack";
 import { Pokemon } from "../pokemon";
+import { Resistances } from "../type/resistances";
+import { EnergyTypes } from "../type/types";
+import { Weaknesses } from "../type/weakness";
 
 export class Charmeleon extends Pokemon{
-    constructor(name, health, type, attacks, resistance, weakness){
-        super(name, health, type, attacks, resistance, weakness);
+    constructor(name: string, health: number, attacks: Attack[],){
+        super(name, health, EnergyTypes.Fire, attacks, Resistances.Electric, Weaknesses.Water);
     }
 }
